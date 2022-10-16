37404fd6befcb97df3cc1f129b5f87cd38ec2caf
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-10-16 19:07:06 -0300

    fix(javascript-ex): feat: my feature branch
    # ChangelogThis PR has a changelog that describes better what will really go to this new version
    
    > Version: 1.1.39

--- examples/javascript-ex/CHANGELOG.md
+++ examples/javascript-ex/CHANGELOG.md
@@ -1,5 +1,5 @@
 
-## 1.1.38
+## 1.1.39
 
 # ChangelogThis PR has a changelog that describes better what will really go to this new version
                 
--- src/infra/resolverBase/index.js
+++ src/infra/resolverBase/index.js
@@ -27,6 +27,7 @@ module.exports = {
             async updateChangelog({ newVersion, commitBody }) {
                 const changelogFilePath = path.resolve(packagePath, "CHANGELOG.md");
                 const changelogBody = commitBody;
+                console.log("aaaaaaaaa")
                 const changelogNewContent = `
 ## ${newVersion}
 
