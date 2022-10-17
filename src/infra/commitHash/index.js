28c55b120a1a55cb58c423d1e854b77f23d6f7cc
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-10-17 13:53:04 -0300

    update

--- src/infra/commitHash/index.js
+++ src/infra/commitHash/index.js
@@ -1,5 +1,5 @@
 const { execSync } = require("child_process");
 
 module.exports = {
-    getCommitHash: ({ cwd }) => execSync("git rev-parse HEAD", { encoding: "utf-8", cwd: cwd }).trim(),
+    getCommitHash: ({ cwd } = {}) => execSync("git rev-parse HEAD", { encoding: "utf-8", cwd: cwd }).trim(),
 }
