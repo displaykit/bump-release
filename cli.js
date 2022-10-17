a1524608bdca99f30da2e87203dc59482fd68e9e
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-10-16 21:25:36 -0300

    update

---
+++ cli.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+// const { execSync } = require('child_process');
+
+// Example: bump-release /release/beta javascript-ex ./examples/javascript-ex javascript
+const input = process.argv[2];
+const name = process.argv[3];
+const projectPath = process.argv[4];
+const resolver = process.argv[5];
+
+const bumpType = input.split('/')[2];
+
+console.log("[@displaykit/releaser]");
+
+const command = `
+yarn dev version ${projectPath} \
+--type=${bumpType} \
+--resolver=${resolver} \
+--name=${name} \
+--github-pull-request-number=${process.env.GITHUB_PR_NUMBER} \
+--github-repo-owner=${process.env.GITHUB_REPO_OWNER} \
+--github-repo-name=${process.env.GITHUB_REPO_NAME} \
+--github-token=${process.env.GITHUB_TOKEN}
+`;
+
+console.log(command);
+
+// const output = execSync(, { encoding: 'utf8' });
