a0ce29957ff29a81eeb8b7677a25b4dd9980d717
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-10-16 22:57:45 -0300

    update

--- src/infra/pushToGitHub/index.js
+++ src/infra/pushToGitHub/index.js
@@ -1,7 +1,7 @@
 const { execSync } = require("child_process");
 
 async function pushToGitHub({ branch }) {
-    const output = execSync(`git push -u origin HEAD:${branch} --follow-tags`, { encoding: "utf-8" });
+    const output = execSync(`git push -u origin HEAD --follow-tags`, { encoding: "utf-8" });
     console.log(output);
     return true;
 }
