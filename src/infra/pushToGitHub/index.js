4da21c8c00e1602d95fea8f8c11c4316fd1fe105
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-10-17 14:06:49 -0300

    fix(dart-ex): Update README.md
    undefined
    
    > Version: 2.1.50

--- src/infra/pushToGitHub/index.js
+++ src/infra/pushToGitHub/index.js
@@ -1,6 +1,6 @@
 const { execSync } = require("child_process");
 
-async function pushToGitHub({ branch }) {
+async function pushToGitHub({ branch, cwd }) {
     const output = execSync(`git push -u origin HEAD --follow-tags`, { encoding: "utf-8", cwd });
     console.log(output);
     return true;
