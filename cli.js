4352b376560063769f82cab00c123ffabb157a71
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-10-17 13:15:00 -0300

    update

--- cli.js
+++ cli.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 const { exec } = require('child_process');
 const fs = require('fs');
+const path = require("path");
+
+console.log('npx installation path', __dirname);
 
 let CONFIG;
-const BUMP_RELEASE_CONFIG_PATH = process.env.BUMP_RELEASE_CONFIG_PATH || './.bump-release.json';
+const BUMP_RELEASE_CONFIG_PATH = process.env.BUMP_RELEASE_CONFIG_PATH || path.resolve('.bump-release.json');
 if(fs.existsSync(BUMP_RELEASE_CONFIG_PATH)) {
     CONFIG = JSON.parse(fs.readFileSync(BUMP_RELEASE_CONFIG_PATH, 'utf8'));
 }
@@ -18,13 +21,13 @@ console.log("Project Name: ", name);
 const projectPath = process.argv[4] || CONFIG[name].path;
 const resolver = process.argv[5]  || CONFIG[name].resolver;
 
-const bumpType = input.split('/')[2];
-const [GITHUB_REPO_OWNER, GITHUB_REPO_NAME] = process.env.GITHUB_REPOSITORY.split('/');
+const bumpType = input?.split('/')[2];
+const [GITHUB_REPO_OWNER, GITHUB_REPO_NAME] = process.env.GITHUB_REPOSITORY?.split('/') || [];
 
 console.log("[@displaykit/releaser]");
 
 const command = `
-yarn start version ${projectPath} \
+yarn --cwd ${__dirname} start version ${projectPath} \
 --type=${bumpType} \
 --resolver=${resolver} \
 --name=${name} \
@@ -36,8 +39,8 @@ yarn start version ${projectPath} \
 
 console.log(command);
 
-const run = exec(command, { encoding: 'utf8' });
+// const run = exec(command, { encoding: 'utf8' });
 
-run.stdout.on('data', (data) => {
-    console.log(data);
-});
+// run.stdout.on('data', (data) => {
+//     console.log(data);
+// });
