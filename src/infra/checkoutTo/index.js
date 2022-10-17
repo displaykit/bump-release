c007c7b36220172497db6bb422652f89d53a58b8
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-10-16 22:14:01 -0300

    update

--- src/infra/checkoutTo/index.js
+++ src/infra/checkoutTo/index.js
@@ -1,7 +1,8 @@
 const { execSync } = require("child_process");
 
 async function checkoutTo({ branch }) {
-    execSync(`git checkout ${branch}`, { encoding: "utf-8" });
+    const output = execSync(`git checkout ${branch}`, { encoding: "utf-8" });
+    console.log(output);
     return true
 }
 
